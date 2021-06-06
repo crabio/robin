940842d6d4ac878e35eab1399342092307faeb63
iakrevetkho <vanoschukin@yandex.ru>
2021-06-06 19:16:27 +0300

    optimize script

--- scripts/build_proto.sh
+++ scripts/build_proto.sh
@@ -3,8 +3,5 @@
 # Go to script directory
 cd $(dirname $0)
 
-# Go to root project dir
-cd ..
-ls proto
 # Generate protobuf files
-protoc -I=proto --go_out=internal proto/*
+protoc -I=../proto --go_out=../internal ../proto/*
