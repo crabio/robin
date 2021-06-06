46864ec925e5ccefb8f1e125facc1b78b0db6ea0
iakrevetkho <vanoschukin@yandex.ru>
2021-06-06 22:43:41 +0300

    fix config

--- internal/config/config.go
+++ internal/config/config.go
@@ -4,8 +4,8 @@ type Config struct {
 	NATS struct {
 		Hostname string `required:"true" default:"localhost"`
 		Request  struct {
-			Subject string `default:"robin"`
-			Queue   string `default:"auth-request"`
+			Subject string `default:"auth-request"`
+			Queue   string `default:"robin"`
 		}
 	}
 }
